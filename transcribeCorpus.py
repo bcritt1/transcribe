51b75bc8138b6d8248bf45657dae450da83d2015
bcritt1 <bcritt@stanford.edu>
2023-05-04 10:59:13 -0700

    add dir

---
+++ transcribeCorpus.py
@@ -0,0 +1,27 @@
+import librosa
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+#load pre-trained model and tokenizer
+tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+pathAudio = "/home/users/bcritt/audio/"
+files = librosa.util.find_files(pathAudio, ext=['wav'])
+files = np.asarray(files)
+info=[]
+for y in files:
+	data = librosa.load(y, sr=16000)
+	data = data[0]
+	input_values = tokenizer(data, return_tensors = 'pt').input_values
+	logits = model(input_values).logits
+	predicted_ids = torch.argmax(logits, dim =-1)
+	transcriptions = tokenizer.decode(predicted_ids[0])
+	info.append(transcriptions)
+
+df = pd.DataFrame(info)
+df.to_csv("/scratch/users/bcritt/outputs/transcribe.csv")
---
+++ transcribeSingle.py
@@ -0,0 +1,24 @@
+import librosa
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+import pandas as pd
+
+#load pre-trained model and tokenizer
+tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+#load any audio file of your choice
+speech, rate = librosa.load("/Users/bcritt/Documents/StanfordProjects/Corpora/audio/recording1.wav",sr=16000)
+
+input_values = tokenizer(speech, return_tensors = 'pt').input_values
+
+#Store logits (non-normalized predictions)
+logits = model(input_values).logits
+
+#Store predicted id's
+predicted_ids = torch.argmax(logits, dim =-1)
+
+#decode the audio to generate text
+transcriptions = tokenizer.decode(predicted_ids[0])
+df = pd.DataFrame([transcriptions])
+df.to_csv("/Users/bcritt/Documents/StanfordProjects/Corpora/audio/transcribe.csv")
